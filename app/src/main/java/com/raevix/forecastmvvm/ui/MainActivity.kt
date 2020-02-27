8e4b9e9e231eebab84b9bb605f6e0a9ca6d4d5bc
Willyelns Germano <willyelns.xavier@nexxera.com>
2020-02-27 13:02:47 -0300

    Initial Project * Add Dependencies * Add Fragments
    ## TODO:
    * Add vectors
    * Create the 'page fragments'

---
+++ app/src/main/java/com/raevix/forecastmvvm/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.raevix.forecastmvvm.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.raevix.forecastmvvm.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
